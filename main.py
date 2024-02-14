0fcfcbd2fd3ded952111de05fa4fb9b3bacc79e1
bytegun <santa941010@gmail.com>
2024-02-14 04:32:50 -0800

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ try:
 		text = f.readline()
 		num = [int(x) for x in text.split() if x.isdigit()][0]
 		num += 1
-	
+	print(num)
 	if num<= 1024:
 		with open("status.md" , 'w') as f:
 			if num in range(16):
--- status.md
+++ status.md
@@ -1,1 +1,1 @@
-0 pull requests merged<br>Currently:<br>![pull-shark](images/pull-shark-silver.png)
+6 pull requests merged<br>Currently:<br>![pull-shark](images/pull-shark-default.png)
